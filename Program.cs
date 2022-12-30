7344ba934aba86514b68f03d76de3b5293ddf310
KhalidMohamed57 <121644086+KhalidMohamed57@users.noreply.github.com>
2022-12-30 16:00:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("ENTER 1St NUMBER");
+        int a = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("ENTER 2Scd NUMBER");
+        int s = int.Parse(Console.ReadLine()!);
+        for (int i = a; i <= s; i++)
+        { int divisors = 0;
+            for ( int j=1;j<=i; j++ )
+            if (i%j==0) { divisors++; }
+            if (divisors == 2)
+            {
+                Console.WriteLine(i + "is prime number");
+            }
+
+
+
+
+        }
+
+    }
+}
